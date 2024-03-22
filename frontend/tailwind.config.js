22766ad1e4483d4fc5693323d034eb772d94e2f5
pgm-jefdenie <jefdeniel@icloud.com>
2024-03-22 21:31:12 +0100

    🔧 Set default tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000',
+      white: '#fff',
+    },
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
   },
   plugins: [],
 };
