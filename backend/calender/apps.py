e0043a6a3c007e5a73627ecced246491e38e6c8e
pgm-jefdenie <jefdeniel@icloud.com>
2024-04-09 09:59:00 +0200

    🎉 Reinstall backend

--- backend/calender/apps.py
+++ backend/calender/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MainConfig(AppConfig):
+class CalenderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'main'
+    name = 'calender'
