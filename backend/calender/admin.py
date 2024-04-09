6a5eff860bec14d9c566d521e3f3656bbf5ca776
pgm-jefdenie <jefdeniel@icloud.com>
2024-04-09 10:22:35 +0200

    🗃️ Create models and migrations for events, reminder and calenders

--- backend/calender/admin.py
+++ backend/calender/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
+# This is the admin interface that Django provides by default
 # Register your models here.
+
+from .models import Event, Reminder, Calender
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "description",
+        "start_time",
+        "end_time",
+        "location",
+        "creator",
+        "created_at",
+        "updated_at",
+    )
+
+
+class ReminderAdmin(admin.ModelAdmin):
+    list_display = (
+        "event",
+        "time",
+        "user",
+        "created_at",
+        "updated_at",
+    )
+
+
+class CalenderAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "created_at", "updated_at", "display_events")
+
+    def display_events(self, obj):
+        return ", ".join([event.title for event in obj.events.all()])
+
+    display_events.short_description = "Events"
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Reminder, ReminderAdmin)
+admin.site.register(Calender, CalenderAdmin)
--- backend/calender/models.py
+++ backend/calender/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    location = models.CharField(max_length=200)
+    creator = models.ForeignKey(
+        "auth.User", related_name="events", on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Reminder(models.Model):
+    event = models.ForeignKey(Event, related_name="reminders", on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    user = models.ForeignKey(
+        "auth.User", related_name="reminders", on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Reminder for {self.event.title} - {self.user.username} - {self.time.strftime("%Y-%m-%d %H:%M")}'
+
+
+class Calender(models.Model):
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey(
+        "auth.User", related_name="calenders", on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    events = models.ManyToManyField(Event, related_name="calenders")
+
+    def __str__(self):
+        return self.name
