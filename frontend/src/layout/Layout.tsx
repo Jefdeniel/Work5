689fe16aec1a18e07097220954fa0adc2698e002
pgm-jefdenie <jefdeniel@icloud.com>
2024-04-22 21:27:12 +0200

    🐛 fix build issue

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -6,6 +6,7 @@ const Layout = ({ title }: Props) => {
   return (
     <div className="d-flex flex-row p-0" style={{ minHeight: '100%' }}>
       layout
+      {title}
     </div>
   );
 };
---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <div className="d-flex flex-row p-0" style={{ minHeight: '100%' }}>
+      Register
+    </div>
+  );
+};
+
+export default Register;
