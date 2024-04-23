2e2e9ec24af8c640fc45508ab0d354d55e1c7127
pgm-jefdenie <jefdeniel@icloud.com>
2024-04-23 13:59:09 +0200

    Loading screen fallback

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,17 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import './i18n.js';
+import LoadingScreen from './components/ui/Loading/LoadingScreen';
 
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <React.Suspense fallback={<LoadingScreen />}>
+        <App />
+      </React.Suspense>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
