979540ca7a693fe4768de34ee9d7003cd47260c4
pgm-jefdenie <jefdeniel@icloud.com>
2024-04-25 15:26:55 +0200

    ✨ Work on setting selectors

---
+++ frontend/src/components/settings/agendaView/WeekendVisibiltySelector.tsx
@@ -0,0 +1,61 @@
+import { useTranslation } from 'react-i18next';
+import { FieldMetaState } from 'react-final-form';
+import Select from '../../ui/Select/Select';
+
+interface Props {
+  initialDisplay?: string;
+  value?: boolean;
+  onChange: (Theme: boolean) => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  meta?: FieldMetaState<any>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [x: string]: any;
+}
+
+const OPTIONS = [
+  {
+    title: 'settings:agendaView.show',
+    value: true,
+  },
+  {
+    title: 'settings:agendaView.dontShow',
+    value: false,
+  },
+];
+
+const WeekendVisbilityOnSelector = ({
+  initialDisplay,
+  value,
+  onChange,
+  meta,
+  ...rest
+}: Props) => {
+  const { t } = useTranslation(['settings']);
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    onChange(e.target.value === 'true');
+  };
+
+  const translatedOptions = OPTIONS.map((option) => ({
+    title: t(option.title),
+    value: option.value,
+    selected: option.value === value,
+  }));
+
+  return (
+    <Select
+      title={t('settings:agendaView.weekendVisibilityToggle')}
+      description={t('settings:agendaView.weekendVisibilityToggleDescription')}
+      defaultValue={initialDisplay}
+      onChange={handleChange}
+      options={translatedOptions.map((option) => ({
+        ...option,
+        value: option.value.toString(),
+      }))}
+      meta={meta}
+      {...rest}
+    />
+  );
+};
+
+export default WeekendVisbilityOnSelector;
