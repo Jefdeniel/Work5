20bb52368c856476ec8b0846ca22267654c1e430
pgm-jefdenie <jefdeniel@icloud.com>
2024-04-25 15:27:57 +0200

    🚧 Unfinished event reminder

---
+++ frontend/src/components/settings/notifications/EventReminder.tsx
@@ -0,0 +1,14 @@
+import Switch from '../../ui/Switch/Switch';
+
+const EventReminder = () => {
+  return (
+    <Switch
+      title="Event Reminder"
+      description="Enable event reminders"
+      checked={true}
+      onChange={() => {}}
+    />
+  );
+};
+
+export default EventReminder;
