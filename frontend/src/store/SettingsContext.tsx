acb82186213a8b4ffb2c1a5bad143fb68e00e142
pgm-jefdenie <jefdeniel@icloud.com>
2024-04-25 15:28:28 +0200

    🔧 Expend settings context

--- frontend/src/store/SettingsContext.tsx
+++ frontend/src/store/SettingsContext.tsx
@@ -6,6 +6,14 @@ interface SettingsContextType {
   setLanguage: (lang: string) => void;
   selectedTimezone?: string | ITimezoneOption;
   setSelectedTimezone: (selectedTimezone: string) => void;
+  timeFormat?: string;
+  setTimeFormat: (timeFormat: string) => void;
+  theme?: string;
+  setTheme: (theme: string) => void;
+  weekStartsOn?: number;
+  setWeekStartsOn: (weekStartsOn: number) => void;
+  weekendVisibility?: boolean;
+  setWeekendVisibility: (weekendVisibility: boolean) => void;
 }
 
 export const SettingsContext = createContext<SettingsContextType>({
@@ -15,6 +23,18 @@ export const SettingsContext = createContext<SettingsContextType>({
   selectedTimezone: 'UTC',
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   setSelectedTimezone: () => {},
+  timeFormat: '24h',
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setTimeFormat: () => {},
+  theme: 'light',
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setTheme: () => {},
+  weekStartsOn: 1,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setWeekStartsOn: () => {},
+  weekendVisibility: true,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setWeekendVisibility: () => {},
 });
 
 export const SettingsContextProvider = ({
@@ -28,6 +48,10 @@ export const SettingsContextProvider = ({
   const [selectedTimezone, setSelectedTimezone] = useState<ITimezone>(
     Intl.DateTimeFormat().resolvedOptions().timeZone
   );
+  const [timeFormat, setTimeFormat] = useState<string>('24h');
+  const [theme, setTheme] = useState<string>('light');
+  const [weekStartsOn, setWeekStartsOn] = useState<number>(1);
+  const [weekendVisibility, setWeekendVisibility] = useState<boolean>(true);
 
   useEffect(() => {
     void i18n.changeLanguage(language);
@@ -38,6 +62,14 @@ export const SettingsContextProvider = ({
     setLanguage,
     selectedTimezone,
     setSelectedTimezone,
+    timeFormat,
+    setTimeFormat,
+    theme,
+    setTheme,
+    weekStartsOn,
+    setWeekStartsOn,
+    weekendVisibility,
+    setWeekendVisibility,
   };
 
   return (
