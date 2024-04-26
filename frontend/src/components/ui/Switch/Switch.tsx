3368b0f69efa7a361cbecb96a54d0c16794d6a70
pgm-jefdenie <jefdeniel@icloud.com>
2024-04-26 13:51:06 +0200

    💄 fix switch

--- frontend/src/components/ui/Switch/Switch.tsx
+++ frontend/src/components/ui/Switch/Switch.tsx
@@ -1,7 +1,6 @@
 import Col from '../Flex/Col';
 import Row from '../Flex/Row';
 import './Switch.css';
-
 interface Props {
   title?: string;
   description?: string;
@@ -22,7 +21,7 @@ const Switch = ({ title, description, checked, onChange }: Props) => {
           <small className="description">{description}</small>
         </Col>
         <Col>
-          <label className="relative inline-flex cursor-pointer items-center">
+          <label className="switch-label">
             <input
               id="switch"
               type="checkbox"
@@ -30,10 +29,7 @@ const Switch = ({ title, description, checked, onChange }: Props) => {
               checked={checked}
               onChange={handleChange}
             />
-            <label htmlFor="switch" className="hidden">
-              {title}
-            </label>
-            <div className="peer h-6 w-11 rounded-full border bg-slate-200 after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-slate-800 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-green-300"></div>
+            <span className="switch-slider"></span>
           </label>
         </Col>
       </Row>
