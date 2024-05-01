e8e116617ebfd4d76ad4c64004bc7e3e62da867c
pgm-jefdenie <jefdeniel@icloud.com>
2024-05-01 17:44:48 +0200

    🏷️ Create color type

---
+++ frontend/src/@types/Colors.ts
@@ -0,0 +1,14 @@
+export enum Colors {
+  Primary100 = '#d0f0fa',
+  Primary200 = '#add9e2',
+  Primary300 = '#05a8bd',
+  Secondary100 = '#2db6b1',
+  Secondary200 = '#1e8983',
+  Secondary300 = '#0f4e4c',
+  Error100 = '#ff8d91',
+  Error200 = '#e3262e',
+  Error300 = '#ff0000',
+  Gray100 = '#9191A6',
+  Bright = '#f9f9f9',
+  Dark = '#2b4450',
+}
