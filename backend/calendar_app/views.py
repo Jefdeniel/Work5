69b5dbaf7d0b93710b8f97bb385fb60ace2605b7
pgm-jefdenie <jefdeniel@icloud.com>
2024-05-03 11:51:50 +0200

    🗃️ Add calendar endpoints

--- backend/calendar_app/views.py
+++ backend/calendar_app/views.py
@@ -1,78 +1,62 @@
-# if rest_frameworks imports can't be found: Ctrl + Shift + P -> Type and select 'Python: Select Interpreter'
-# Click and selecton the Microsoft Python Interpreter
+from .views.calendar_views import CalendarListView, CalendarDetailView
 
 
-# from .utils import access_token_IsAuthenticated
-from rest_framework import viewsets
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .serializers import EventSerializer, ReminderSerializer, CalendarSerializer
-from .models import Event, Reminder, Calendar
-
-import json
-from django.contrib.auth import authenticate, login, logout
-from django.http import JsonResponse
-from django.views.decorators.csrf import ensure_csrf_cookie
-from django.views.decorators.http import require_POST
+# @api_view(["GET"])
+# def get_calendars(request):
+#     calendars = Calendar.objects.all()
+#     serializer = CalendarSerializer(calendars, many=True)
+#     return Response(serializer.data)
 
 
-# Create your views here.
+# @api_view(["POST"])
+# def login_view(request):
+#     data = json.loads(request.body)
+#     username = data.get("username")
+#     password = data.get("password")
+
+#     if username is None or password is None:
+#         return JsonResponse(
+#             {"error": "Please provide both username and password"}, status=400
+#         )
+#     user = authenticate(request, username=username, password=password)
+#     if user is None:
+#         return JsonResponse({"invalid credentials": False}, status=400)
+#     login(request, user)
+#     return JsonResponse({"successful login": True})
 
 
-class EventView(viewsets.ModelViewSet):
-    serializer_class = EventSerializer
-    queryset = Event.objects.all()
+# @require_POST
+# def login_view(request):
+#     data = json.loads(request.body)
+#     username = data.get("username")
+#     password = data.get("password")
+
+#     if username is None or password is None:
+#         return JsonResponse(
+#             {"error": "Please provide both username and password"}, status=400
+#         )
+#     user = authenticate(request, username=username, password=password)
+#     if user is None:
+#         return JsonResponse({"invalid credentials": False}, status=400)
+#     login(request, user)
+#     return JsonResponse({"successful login": True})
 
 
-class ReminderView(viewsets.ModelViewSet):
-    serializer_class = ReminderSerializer
-    queryset = Reminder.objects.all()
+# def logout_view(request):
+#     if not request.user.is_authenticated:
+#         return JsonResponse({"error": "You are not logged in"}, status=400)
+#     logout(request)
+#     return JsonResponse({"successful logout": True})
 
 
-class CalendarView(viewsets.ModelViewSet):
-    serializer_class = CalendarSerializer
-    queryset = Calendar.objects.all()
+# @ensure_csrf_cookie
+# def session_view(request):
+#     if not request.user.is_authenticated:
+#         return JsonResponse({"isAuthenticated": False})
+#     return JsonResponse({"isAuthenticated": True})
 
 
-@api_view(["GET"])
-def get_calendars(request):
-    calendars = Calendar.objects.all()
-    serializer = CalendarSerializer(calendars, many=True)
-    return Response(serializer.data)
-
-
-@require_POST
-def login_view(request):
-    data = json.loads(request.body)
-    username = data.get("username")
-    password = data.get("password")
-
-    if username is None or password is None:
-        return JsonResponse(
-            {"error": "Please provide both username and password"}, status=400
-        )
-    user = authenticate(request, username=username, password=password)
-    if user is None:
-        return JsonResponse({"invalid credentials": False}, status=400)
-    login(request, user)
-    return JsonResponse({"successful login": True})
-
-
-def logout_view(request):
-    if not request.user.is_authenticated:
-        return JsonResponse({"error": "You are not logged in"}, status=400)
-    logout(request)
-    return JsonResponse({"successful logout": True})
-
-
-@ensure_csrf_cookie
-def session_view(request):
-    if not request.user.is_authenticated:
-        return JsonResponse({"isAuthenticated": False})
-    return JsonResponse({"isAuthenticated": True})
-
-
-def whoami_view(request):
-    if not request.user.is_authenticated:
-        return JsonResponse({"isAuthenticated": None})
-    return JsonResponse({"isAuthenticated": request.user.username})
+# def whoami_view(request):
+#     if not request.user.is_authenticated:
+#         return JsonResponse({"isAuthenticated": None})
+#     return JsonResponse({"isAuthenticated": request.user.username})
