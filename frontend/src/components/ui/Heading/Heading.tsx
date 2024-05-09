cefc5ba3e0e289f722da86508122673c61c85841
Kyandroo <kyandevo@student.arteveldehs.be>
2024-05-09 10:53:15 +0200

    💄 Edit heading styling with variable fonts

--- frontend/src/components/ui/Heading/Heading.tsx
+++ frontend/src/components/ui/Heading/Heading.tsx
@@ -29,37 +29,37 @@ export default function Heading({
   switch (level) {
     case 1:
       return (
-        <h1 style={style} className={getClasses()}>
+        <h1 style={style} className={`heading heading--sm ${getClasses()}`}>
           {children}
         </h1>
       );
     case 2:
       return (
-        <h2 style={style} className={getClasses()}>
+        <h2 style={style} className={`heading heading--md ${getClasses()}`}>
           {children}
         </h2>
       );
     case 3:
       return (
-        <h3 style={style} className={getClasses()}>
+        <h3 style={style} className={`heading heading--lg ${getClasses()}`}>
           {children}
         </h3>
       );
     case 4:
       return (
-        <h4 style={style} className={getClasses()}>
+        <h4 style={style} className={`heading ${getClasses()}`}>
           {children}
         </h4>
       );
     case 5:
       return (
-        <h5 style={style} className={getClasses()}>
+        <h5 style={style} className={`heading ${getClasses()}`}>
           {children}
         </h5>
       );
     case 6:
       return (
-        <h6 style={style} className={getClasses()}>
+        <h6 style={style} className={`heading ${getClasses()}`}>
           {children}
         </h6>
       );
