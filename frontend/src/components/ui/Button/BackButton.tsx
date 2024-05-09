9eb6ddfcf6437a0192c57e49eba6b8ed472c77e0
Kyandroo <kyandevo@student.arteveldehs.be>
2024-05-09 10:52:28 +0200

    💄 Edit backbuttons styling

--- frontend/src/components/ui/Button/BackButton.tsx
+++ frontend/src/components/ui/Button/BackButton.tsx
@@ -1,4 +1,4 @@
-import './Button.module.scss';
+import './Button.scss';
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text?: string;
@@ -8,13 +8,13 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 const BackButton = ({ text, className }: Props) => {
   const button = (
     <button
-      className={`button ${className}`}
+      className={`btn btn--bordered ${className}`}
       onClick={() => {
         window.history.back();
       }}
     >
       <svg
-        className="button__icon"
+        className="btn__icon"
         width="14"
         height="18"
         fill="none"
@@ -22,7 +22,7 @@ const BackButton = ({ text, className }: Props) => {
       >
         <path
           d="M.683 10.271c-.91-.702-.91-1.844 0-2.547l9.33-7.197c.67-.517 1.67-.669 2.544-.388C13.43.42 14 1.073 14 1.804v14.394c0 .725-.569 1.383-1.443 1.664-.875.281-1.873.124-2.544-.388l-9.33-7.197v-.006Z"
-          fill="#1E1E20"
+          fill="var(--sa-primary-950)"
         />
       </svg>
 
