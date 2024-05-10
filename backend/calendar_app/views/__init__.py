6c386081501778126cc5c4c6c3a89665c5e635c6
pgm-jefdenie <jefdeniel@icloud.com>
2024-05-10 15:35:56 +0200

    🔥 delete shit

--- backend/calendar_app/views/__init__.py
+++ backend/calendar_app/views/__init__.py
@@ -5,20 +5,3 @@ from .notification_views import NotificationViewSet
 from .reminder_views import ReminderViewSet
 from .settings_views import UserSettingsViewSet
 from .user_views import CustomUserViewSet
-
-{
-    "password": "password",
-    "last_login": "2024-05-10T12:40:46.960Z",
-    "is_superuser": true,
-    "username": "Jef",
-    "first_name": "string",
-    "last_name": "string",
-    "email": "jefdeniel@icloud.com",
-    "is_staff": true,
-    "is_active": true,
-    "date_joined": "2024-05-10T12:40:46.960Z",
-    "birthday": "2024-05-10",
-    "role": "ADMIN",
-    "groups": [0],
-    "user_permissions": [0],
-}
