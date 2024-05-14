e42501e120300df01412857244a8a9db251fe7e8
pgm-jefdenie <jefdeniel@icloud.com>
2024-05-14 10:35:46 +0200

    🚀 deploy stuff

--- frontend/src/pages/SharingHubPage.tsx
+++ frontend/src/pages/SharingHubPage.tsx
@@ -1,27 +1,23 @@
 import { useTranslation } from 'react-i18next';
 import useSetTitle from '../hooks/setTitle';
-import { Row } from 'react-bootstrap'; 
+import { Row } from 'react-bootstrap';
 import Heading from '../components/ui/Heading/Heading';
-import Button from '../components/ui/Button/Button';
 
 const SharingHubPage = () => {
-  const { t } = useTranslation(['calendar']); 
+  const { t } = useTranslation(['calendar']);
   useSetTitle(t('calendar:sharing-hub.title'));
 
   return (
-    <> 
+    <>
       <Row>
         <Heading level={1} className="clr-primary mb-small">
-          {t('calendar:sharing-hub.title')} 
+          {t('calendar:sharing-hub.title')}
         </Heading>
-        <p className="mb-large">
-          {t('calendar:sharing-hub.description')}
-        </p>
+        <p className="mb-large">{t('calendar:sharing-hub.description')}</p>
       </Row>
 
       <Row>
         <p>{t('calendar:sharing-hub.usersInAgenda')}</p>
-        {/* <Button className="btn--bordered-danger" isBig={false} text="Invite"/> */}
       </Row>
 
       <Row>
