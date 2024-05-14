c63d73fb5d4805074a892ccde83e81c69932c7b1
pgm-jefdenie <jefdeniel@icloud.com>
2024-05-14 09:52:39 +0200

    ♻️ Fetch and refactor notificationList

---
+++ frontend/src/components/calendar/notifications/NotificationList.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import NotificationCard from '../../ui/NotificationCard/NotificationCard';
+import useFetch from '../../../hooks/useFetch';
+import { DateTime } from 'luxon';
+import { Row } from 'react-bootstrap';
+
+const NotificationList = () => {
+  const [notifications, setNotifications] = useState([]);
+  const { fetchData: getNotifications } = useFetch('GET', ['notifications']);
+
+  useEffect(() => {
+    getNotifications()
+      .then((response) => {
+        if (!response.ok) {
+          toast.error('Failed to fetch notifications');
+          return null;
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data) {
+          setNotifications(data);
+          console.log('Notifications fetched:', data);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching notifications:', error);
+        toast.error('Error fetching notifications');
+      });
+  }, []);
+
+  return (
+    <Row>
+      {notifications.map((notification) => {
+        const notificationStart = DateTime.fromISO(
+          notification.date_start
+        ).toFormat('HH:mm');
+        const notificationStop = DateTime.fromISO(
+          notification.date_stop
+        ).toFormat('HH:mm');
+
+        return (
+          <NotificationCard
+            key={notification.id}
+            title={notification.title}
+            timeFrom={notificationStart}
+            timeTo={notificationStop}
+            isNew={true} // TODO: Implement logic (with passing states and pushing to db ) for new notifications
+          />
+        );
+      })}
+    </Row>
+  );
+};
+
+export default NotificationList;
--- frontend/src/pages/NotificationPage.tsx
+++ frontend/src/pages/NotificationPage.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+
 import { Colors } from '../@types/Colors';
-import ColorPicker from '../components/calendar/notifications/ColorPicker/ColorPicker';
-import CircleWithTitle from '../components/ui/Circle/CircleWithTitle';
+import useSetTitle from '../hooks/setTitle';
+
 import Button from '../components/ui/Button/Button';
 import Heading from '../components/ui/Heading/Heading';
-import NotificationCard from '../components/ui/NotificationCard/NotificationCard';
-import { useTranslation } from 'react-i18next';
-import useSetTitle from '../hooks/setTitle';
+import CircleWithTitle from '../components/ui/Circle/CircleWithTitle';
+import NotificationList from '../components/calendar/notifications/NotificationList';
+import ColorPicker from '../components/calendar/notifications/ColorPicker/ColorPicker';
 
 const NotificationPage = () => {
   const { t } = useTranslation(['calendar']);
@@ -32,20 +34,15 @@ const NotificationPage = () => {
 
   return (
     <div>
-      <Heading
-        className={`clr-primary mb-small`}
-        level={1}
-      >
+      <Heading className={`clr-primary mb-small`} level={1}>
         {t('calendar:notifications.title')}
       </Heading>
 
-      <p className={`mb-large`}>
-        {t('calendar:notifications.description')}
-      </p>
+      <p className={`mb-large`}>{t('calendar:notifications.description')}</p>
 
       <div className={`mb-xlarge`}>
         <span className={`mb-small`}>
-        {t('calendar:notifications.clickOnCircle')}
+          {t('calendar:notifications.clickOnCircle')}
         </span>
 
         <Row className={`notifications-top`}>
@@ -58,7 +55,7 @@ const NotificationPage = () => {
             {yourColorPickerVisible && (
               <ColorPicker
                 color={yourColor}
-              title={t('calendar:notifications.forYou')}
+                title={t('calendar:notifications.forYou')}
                 onChange={handleYourColorChange}
               />
             )}
@@ -86,16 +83,7 @@ const NotificationPage = () => {
           </Col>
         </Row>
       </div>
-
-      <section>
-        <NotificationCard
-          title="Person created the event “Easter party with family” on
-            26/04/24"
-            timeFrom='3 PM'
-            timeTo='4 PM'
-          isNew={true}
-        />
-      </section>
+      <NotificationList />
     </div>
   );
 };
