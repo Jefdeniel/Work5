df19ba4d9b16e9968bb06201c5562e7e3e9a3263
pgm-jefdenie <jefdeniel@icloud.com>
2024-05-14 22:37:21 +0200

    🚧 Fuck around with profilePageHeader

--- frontend/src/components/settings/account/profile/ProfilePageHeader.tsx
+++ frontend/src/components/settings/account/profile/ProfilePageHeader.tsx
@@ -1,10 +1,27 @@
-import { Row } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import { Colors } from '../../../../@types/Colors';
+import Heading from '../../../ui/Heading/Heading';
 
+// TODO: make dynamic (first authorisation)
 const ProfilePageHeader = () => {
   return (
-    <Row className="--br-lg" style={{ backgroundColor: Colors.Primary200 }}>
-      profile header
+    <Row className="p-4 --br-lg" style={{ backgroundColor: Colors.Primary200 }}>
+      <Col>
+        <Heading level={1}>John Doe</Heading>
+        <Heading level={2} style={{ color: Colors.Primary400 }}>
+          johndoe@gmail.com
+        </Heading>
+      </Col>
+      <Col className="flexCenter">
+        <img
+          src="/logo.jpg"
+          alt="Profile"
+          height={100}
+          width={100}
+          style={{ objectFit: 'cover', borderRadius: 'var(--br-full)' }}
+          className="profile-pic"
+        />
+      </Col>
     </Row>
   );
 };
