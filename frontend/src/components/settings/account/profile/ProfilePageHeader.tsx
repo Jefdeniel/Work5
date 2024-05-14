6f2d1e64537cb669904ad4bc096a4f7b2e58c080
pgm-jefdenie <jefdeniel@icloud.com>
2024-05-14 15:37:00 +0200

    🚧 Will do this later, maybe after auth

---
+++ frontend/src/components/settings/account/profile/ProfilePageHeader.tsx
@@ -0,0 +1,12 @@
+import { Row } from 'react-bootstrap';
+import { Colors } from '../../../../@types/Colors';
+
+const ProfilePageHeader = () => {
+  return (
+    <Row className="--br-lg" style={{ backgroundColor: Colors.Primary200 }}>
+      profile header
+    </Row>
+  );
+};
+
+export default ProfilePageHeader;
