84998ecd5023cd267b559fd3a88b58b55eeba9ff
pgm-jefdenie <jefdeniel@icloud.com>
2024-05-14 21:05:36 +0200

    🐛 Fix routing layout bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
 import { Settings } from 'luxon';
 import { useTranslation } from 'react-i18next';
 import { Navigate, Route, Routes } from 'react-router-dom';
+
 import './App.scss';
-import CalendarLayout from './layout/CalendarLayout';
+
 import Layout from './layout/Layout';
+import ProfilePage from './pages/ProfilePage';
 import Calendar from './pages/Calendar/Calendar';
-import CreateCalendar from './pages/Calendar/CreateCalendar';
 import CustomizePage from './pages/CustomizePage';
+import SharingHubPage from './pages/SharingHubPage';
+import CalendarLayout from './layout/CalendarLayout';
 import NotificationPage from './pages/NotificationPage';
 import SettingsPage from './pages/Settings/SettingsPage';
-import SharingHubPage from './pages/SharingHubPage';
-import ProfilePage from './pages/ProfilePage';
+import CreateCalendar from './pages/Calendar/CreateCalendar';
 
 function App() {
   const { i18n } = useTranslation();
@@ -18,20 +20,28 @@ function App() {
 
   return (
     <Routes>
+      {/* Main layout for general pages */}
       <Route element={<Layout />}>
         <Route path="/" element={<Navigate to="/calendar/create" replace />} />
         <Route path="profile" element={<ProfilePage />} />
         <Route path="settings" element={<SettingsPage />} />
       </Route>
-      {/* Calendars */}
-      <Route element={<CalendarLayout />}>
-        <Route path="calendar">
-          <Route path="overview" element={<Calendar />} />
-          <Route path="create" element={<CreateCalendar />} />
-          <Route path="notifications" element={<NotificationPage />} />
-          <Route path="sharing-hub" element={<SharingHubPage />} />
-          <Route path="customize" element={<CustomizePage />} />
-        </Route>
+
+      {/* Calendar specific routes with CalendarLayout */}
+      <Route path="/calendar" element={<CalendarLayout />}>
+        <Route path="overview" element={<Calendar />} />
+        <Route path="create" element={<CreateCalendar />} />
+      </Route>
+
+      {/* Account specific routes under /calendar but using a different layout */}
+      <Route path="/calendar/notifications" element={<Layout />}>
+        <Route index element={<NotificationPage />} />
+      </Route>
+      <Route path="/calendar/sharing-hub" element={<Layout />}>
+        <Route index element={<SharingHubPage />} />
+      </Route>
+      <Route path="/calendar/customize" element={<Layout />}>
+        <Route index element={<CustomizePage />} />
       </Route>
     </Routes>
   );
