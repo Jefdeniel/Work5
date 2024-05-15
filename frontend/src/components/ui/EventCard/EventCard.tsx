dcf3c8b114ae2028fc32e0da1e3f298fc863c4f3
Kyandroo <kyandevo@student.arteveldehs.be>
2024-05-15 22:12:54 +0200

    💄 Start styling event cards (Need to be finished)

---
+++ frontend/src/components/ui/EventCard/EventCard.tsx
@@ -0,0 +1,15 @@
+import './Card.scss';
+
+interface Props {
+  title: string;
+}
+
+const EventCard = ({ title }: Props) => {
+  return (
+    <div className={`event-card`}>
+      <span>{title}</span>
+    </div>
+  );
+};
+
+export default EventCard;
