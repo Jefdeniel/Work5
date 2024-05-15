2eb2c5fe98378b284c75d266b20c2fc210d34d47
Kyandroo <kyandevo@student.arteveldehs.be>
2024-05-15 22:10:59 +0200

    💄 Start calendar card component (Not finished yet)

---
+++ frontend/src/components/ui/CalendarCard/CalendarCard.tsx
@@ -0,0 +1,27 @@
+import './CalendarCard.scss';
+
+interface Props {
+  img: string;
+  name: string;
+  peopleAvatars: string[];
+}
+
+const CalendarCard = ({ img, name, peopleAvatars }: Props) => {
+  return (
+    <div className="calendar-card">
+      <img src={img} alt={`Thumbnail picture of "${name}" calendar`} />
+
+      <div className="calendar-card__content">
+        <span className="heading heading--md">{name}</span>
+
+        <div className="calendar-card__avatars">
+          {peopleAvatars.map((avatar, index) => (
+            <img key={index} src={avatar} alt="avatar" />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CalendarCard;
