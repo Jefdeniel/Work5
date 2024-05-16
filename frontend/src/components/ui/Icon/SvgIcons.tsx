e614d7d3c4acb2b9f8ee138845e019adfa7d2271
Kyandroo <kyandevo@student.arteveldehs.be>
2024-05-16 18:34:54 +0200

    🍱 Added SvgIcon for Plus SVG

--- frontend/src/components/ui/Icon/SvgIcons.tsx
+++ frontend/src/components/ui/Icon/SvgIcons.tsx
@@ -58,4 +58,24 @@ const CustomizeIcon = ({ className }: Props) => {
   );
 };
 
-export { NotificationIcon, SharingHubIcon, CustomizeIcon };
+const PlusIcon = ({ className }: Props) => {
+  return (
+    <div className={`${className}`}>
+      <svg
+        width="17"
+        height="18"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M10.23 2.23A1.73 1.73 0 0 0 8.5.5a1.73 1.73 0 0 0-1.73 1.73v5.04H1.73A1.73 1.73 0 0 0 0 9c0 .957.774 1.73 1.73 1.73h5.04v5.04a1.73 1.73 0 1 0 3.462 0v-5.04h5.037A1.73 1.73 0 0 0 17 9a1.73 1.73 0 0 0-1.73-1.73h-5.04V2.23Z"
+          fill="var(--sa-primary-400)"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export { NotificationIcon, SharingHubIcon, CustomizeIcon, PlusIcon };
