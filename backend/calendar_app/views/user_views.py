2d83d3f9bf9a65872af359b749919e0aefebbae9
pgm-jefdenie <jefdeniel@icloud.com>
2024-05-16 11:51:10 +0200

    🚀 try stuff

--- backend/calendar_app/views/user_views.py
+++ backend/calendar_app/views/user_views.py
@@ -6,3 +6,6 @@ from ..serializers import CustomUserSerializer
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    get_full_name = CustomUser.get_full_name
+    get_short_name = CustomUser.get_short_name
+    get_username = CustomUser.get_username
