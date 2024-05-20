31a92ca3d767069cb450a776f41b5599dfa2f39c
pgm-jefdenie <jefdeniel@icloud.com>
2024-05-20 23:26:23 +0200

    🚚 Move logout and delete account to profile page

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -5,16 +5,31 @@ import ActionButtonList from '../components/ui/List/ActionButtonList';
 import { CHANGE_OPTIONS_ITEMS, CONNECT_ITEMS } from '../constants/profile';
 import useSetTitle from '../hooks/setTitle';
 import ProfilePageHeader from '../components/settings/account/profile/ProfilePageHeader';
+import Signout from '../components/settings/account/Signout';
+import DeleteAccount from '../components/settings/account/DeleteAccount';
+import DeleteAccountModal from '../components/settings/account/modals/DeleteAccountModal';
+import { useState } from 'react';
 
 const ProfilePage = () => {
   const { t } = useTranslation(['settings']);
   useSetTitle(t('settings:profile.title'));
 
+  const [showDeleteAccountModal, setShowDeleteAccountModal] = useState(false);
+
+  // modals
+  const openDeleteAccountModal = () => {
+    setShowDeleteAccountModal(true);
+  };
+
+  const closeDeleteAccountModal = () => {
+    setShowDeleteAccountModal(false);
+  };
+
   return (
     <>
       <ProfilePageHeader />
 
-      <Row className="mb-large mt-5">
+      <Row className="mb-base mt-large">
         <Heading level={3}>{t('settings:profile.changeOptions')}</Heading>
         <ActionButtonList
           items={CHANGE_OPTIONS_ITEMS}
@@ -22,19 +37,31 @@ const ProfilePage = () => {
         />
       </Row>
 
-      <Row className="mb-large mt-1">
-        <Col>
-          <Heading level={3}>{t('settings:profile.calendars')}</Heading>
-        </Col>
+      <Row className="mb-base mt-large">
+        <Heading level={3}>{t('settings:profile.calendars')}</Heading>
       </Row>
 
-      <Row>
+      <Row className="mb-base mt-large">
         <Heading level={3}>{t('settings:profile.connectAccounts')}</Heading>
         <ActionButtonList
           items={CONNECT_ITEMS}
           className="btn--bordered-primary"
         />
       </Row>
+
+      <Row className="mb-base mt-large">
+        <Heading level={3}>{t('settings:account.title')}</Heading>
+        <Signout className={`btn btn--danger`} />
+
+        <DeleteAccount
+          className={`btn btn--bordered-danger`}
+          onClick={openDeleteAccountModal}
+        />
+      </Row>
+
+      {showDeleteAccountModal && (
+        <DeleteAccountModal onClose={closeDeleteAccountModal} />
+      )}
     </>
   );
 };
