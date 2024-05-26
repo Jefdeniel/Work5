14f8a5655030c124df70e38a5375d6cb3df87c2c
Kyandroo <kyandevo@student.arteveldehs.be>
2024-05-26 13:48:05 +0200

    🎨 Added classname prop to IconButton component

--- frontend/src/components/ui/IconButton/IconButton.tsx
+++ frontend/src/components/ui/IconButton/IconButton.tsx
@@ -4,13 +4,20 @@ interface IconButtonProps {
   style?: React.CSSProperties;
   icon: React.ReactNode;
   filled?: boolean;
+  className?: string;
   onClick: () => void;
 }
 
-const IconButton = ({ style, icon, filled, onClick }: IconButtonProps) => {
+const IconButton = ({
+  style,
+  icon,
+  filled,
+  className,
+  onClick,
+}: IconButtonProps) => {
   return (
     <button
-      className={`icon-button ${filled ? 'btn--filled' : 'btn--outlined'}`}
+      className={`icon-button ${filled ? 'btn--filled' : 'btn--outlined'} ${className}`}
       style={style}
       onClick={onClick}
     >
