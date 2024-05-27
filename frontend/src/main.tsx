2152c80444d0976988efded0933ce4ec59dff92a
Kyandroo <kyandevo@student.arteveldehs.be>
2024-05-27 23:54:07 +0200

    ♻️ Deleted React Suspense

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,25 +1,22 @@
 import './i18n.js';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
 import { BrowserRouter } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.min.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-import './App.scss';
 import App from './App.tsx';
 import { SettingsContextProvider } from './store/SettingsContext.js';
-import LoadingScreen from './components/ui/Loading/LoadingScreen.tsx';
 import { AuthContextProvider } from './store/AuthContext.tsx';
 
-// as per docs, createRoot is the new way to render in React 18
+import './App.scss';
+
+// As per docs, createRoot is the new way to render in React 18
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <AuthContextProvider>
       <SettingsContextProvider>
-        <React.Suspense fallback={<LoadingScreen />}>
-          <App />
-        </React.Suspense>
+        <App />
         <ToastContainer
           position="bottom-right"
           autoClose={5000}
