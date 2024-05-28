5034d621aa37bb2ef0da3f5cf15ff10b1c91a7c7
Kyandroo <kyandevo@student.arteveldehs.be>
2024-05-29 01:04:54 +0200

    ✨ Added full customized toolbar to big calendar with different features

--- frontend/src/components/calendar/BigCalendar/BaseCalendar.tsx
+++ frontend/src/components/calendar/BigCalendar/BaseCalendar.tsx
@@ -1,30 +1,77 @@
 import moment from 'moment';
-import { useMemo, useEffect, useContext } from 'react';
+import { useMemo, useEffect, useContext, useState } from 'react';
 import { Calendar, Views, momentLocalizer } from 'react-big-calendar';
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
+import { Col, Row } from 'react-bootstrap';
 
+import { VIEW_OPTIONS } from '../../../constants/calendar';
 import { SettingsContext } from '../../../store/SettingsContext';
-import { CalendarEvent, Event } from '../../../@types/Events';
+import { Event } from '../../../@types/Events';
 import useFetchedEvents from '../../../hooks/UseFetchedEvents';
 import EventCard from '../../ui/EventCard/EventCard';
+import Button from '../../ui/Button/Button';
+import ProfilePicture from '../../ui/ProfilePicture/ProfilePicture';
+import Icon from '../../ui/Icon/Icon';
+import IconButton from '../../ui/IconButton/IconButton';
+import Input from '../../ui/Input/Input';
+import SmallCalendar from '../../ui/SmallCalendar/SmallCalendar';
 
 import './BaseCalendar.scss';
 import './Calendar.scss';
-import CustomToolbar from './CustomToolbar';
 
 // Calendar step 1: General calendar settings/structure
+// TODO: Warning error on Agenda view
 
-const DnDCalendar = withDragAndDrop<CalendarEvent>(Calendar);
+const DnDCalendar = withDragAndDrop<Event>(Calendar);
 
 interface CalendarProps {
-  onShowEventView: (event: CalendarEvent) => void;
+  onShowEventView: (event: Event) => void;
 }
 
+type Keys = keyof typeof Views;
+
 const BaseCalendar = ({ onShowEventView }: CalendarProps) => {
+  const [view, setView] = useState<(typeof Views)[Keys]>(Views.WEEK);
+  const [date, setDate] = useState(new Date());
+  const [isSmallCalendarOpen, setIsSmallCalendarOpen] = useState(false);
   const { events } = useFetchedEvents();
   const localizer = momentLocalizer(moment);
   const { week_start_day, weekend_visibility, time_format } =
     useContext(SettingsContext);
+
+  const STEP = 15;
+  const TIMESLOTS = 60 / STEP;
+
+  const calendarUsers = [
+    { id: 1, name: 'Person 1', avatar: '/img/test-img.jpg' },
+    { id: 2, name: 'Person 2', avatar: '' },
+    { id: 3, name: 'Person 3', avatar: '/img/test-img.jpg' },
+    { id: 4, name: 'Person 4', avatar: '/img/test-img.jpg' },
+  ];
+
+  const onTodayClick = () => setDate(new Date());
+
+  const onNextClick = () => {
+    if (view === Views.DAY) setDate(moment(date).add(1, 'd').toDate());
+    if (view === Views.WEEK) setDate(moment(date).add(1, 'w').toDate());
+    if (view === Views.MONTH) setDate(moment(date).add(1, 'M').toDate());
+  };
+
+  const onPreviousClick = () => {
+    if (view === Views.DAY) setDate(moment(date).subtract(1, 'd').toDate());
+    if (view === Views.WEEK) setDate(moment(date).subtract(1, 'w').toDate());
+    if (view === Views.MONTH) setDate(moment(date).subtract(1, 'M').toDate());
+  };
+
+  const dateText = useMemo(() => {
+    if (view === Views.DAY) return moment(date).format('MMMM DD YYYY');
+    if (view === Views.WEEK) {
+      const start = moment(date).startOf('week');
+      const end = moment(date).endOf('week');
+      return `${start.format('MMMM DD')} - ${end.format('MMMM DD')}`;
+    }
+    if (view === Views.MONTH) return moment(date).format('MMMM YYYY');
+  }, [date, view]);
 
   useEffect(() => {
     moment.updateLocale('es-es', {
@@ -57,8 +104,6 @@ const BaseCalendar = ({ onShowEventView }: CalendarProps) => {
     []
   );
 
-  console.log(events);
-
   const initProps = useMemo(
     () => ({
       views: weekend_visibility
@@ -79,19 +124,117 @@ const BaseCalendar = ({ onShowEventView }: CalendarProps) => {
     [weekend_visibility, events, time_format, components, onShowEventView]
   );
 
+  const handleSearchFocus = () => {
+    setIsSmallCalendarOpen(true);
+  };
+
+  const handleDateChange = (newDate: Date) => {
+    if (newDate instanceof Date) {
+      setDate(newDate);
+      setIsSmallCalendarOpen(false);
+    }
+  };
+
   return (
-    // <div
-    //   className={`full-calendar ${weekend_visibility ? 'weekend-visible' : 'weekend-hidden'}`}
-    // >
-    <div className={'full-calendar '}>
+    // TODO: Add weekend visibility toggle
+    <div className={'full-calendar'}>
+      {/* <div className={`full-calendar ${weekend_visibility ? 'weekend-visible' : 'weekend-hidden'}`} > */}
+
+      <div className="custom-toolbar">
+        <Row className={`my-4 d-flex align-items-center search-row`}>
+          <Col>
+            {/* TODO: Add translation */}
+            <Input
+              className="toolbar__search"
+              isSearch
+              definePlaceholder="Search"
+              type="search"
+              onFocus={handleSearchFocus}
+            />
+
+            {isSmallCalendarOpen && (
+              <SmallCalendar
+                className={`small-calendar`}
+                onChange={handleDateChange}
+              />
+            )}
+          </Col>
+
+          <Col className={`toolbar-top d-flex justify-content-end`}>
+            <ul className={`toolbar__view-list`}>
+              {VIEW_OPTIONS.map(({ id, label, key }) => (
+                <li key={key}>
+                  <Button
+                    className={`toolbar__view-button ${id === view ? 'active' : ''}`}
+                    onClick={() => setView(id)}
+                  >
+                    {label}
+                  </Button>
+                </li>
+              ))}
+            </ul>
+          </Col>
+        </Row>
+
+        <Row className={`my-3 d-flex align-items-center`}>
+          <Col xs={9} className={`d-flex align-items-center date-title`}>
+            <Button
+              className={`btn--bordered-primary`}
+              text="Today"
+              onClick={onTodayClick}
+            />
+
+            <IconButton
+              className={`click-btn`}
+              icon={<img src="/icons/arrow.svg" alt="Arrow icon" />}
+              onClick={onPreviousClick}
+            />
+
+            <span className={`heading heading--lg fw-bold`}>{dateText}</span>
+
+            <IconButton
+              className={`click-btn`}
+              icon={
+                <img
+                  className={`rotate-180`}
+                  src="/icons/arrow.svg"
+                  alt="Arrow icon"
+                />
+              }
+              onClick={onNextClick}
+            />
+          </Col>
+
+          <Col xs={3} className={`p-0 d-flex justify-content-end`}>
+            <ul className={`m-0 d-flex gap-2`}>
+              {calendarUsers.map((user) => (
+                <li key={user.id}>
+                  {user.avatar ? (
+                    <ProfilePicture
+                      isSmall
+                      src={user.avatar}
+                      alt={`Avatar of ${user.name}`}
+                    />
+                  ) : (
+                    <Icon
+                      src="/icons/user-profile.svg"
+                      alt="User profile icon"
+                    />
+                  )}
+                </li>
+              ))}
+            </ul>
+          </Col>
+        </Row>
+      </div>
+
       <DnDCalendar
         {...initProps}
         localizer={localizer}
-        views={
-          weekend_visibility
-            ? [Views.DAY, Views.WEEK, Views.MONTH, Views.AGENDA]
-            : [Views.DAY, Views.WORK_WEEK, Views.MONTH, Views.AGENDA]
-        }
+        view={view}
+        onView={setView}
+        date={date}
+        onNavigate={setDate}
         defaultView={weekend_visibility ? Views.WEEK : Views.WORK_WEEK}
         onSelectSlot={({ start, end }) => {
           // Logic when selecting a time slot
@@ -106,6 +249,9 @@ const BaseCalendar = ({ onShowEventView }: CalendarProps) => {
         style={{ width: '100%', height: '100%' }} // General props
         components={components}
         selectable
+        toolbar={false}
+        step={STEP}
+        timeslots={TIMESLOTS}
       />
     </div>
   );
