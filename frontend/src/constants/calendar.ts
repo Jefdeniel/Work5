adecaa9487db706550feb2f285c1414705d41af2
Kyandroo <kyandevo@student.arteveldehs.be>
2024-05-29 00:58:44 +0200

    🔧 Added constants for big calendar

---
+++ frontend/src/constants/calendar.ts
@@ -0,0 +1,9 @@
+import { Views } from 'react-big-calendar';
+
+// TODO: Add translations
+export const VIEW_OPTIONS = [
+  { id: Views.DAY, label: 'Day', key: Views.DAY },
+  { id: Views.WEEK, label: 'Week', key: Views.WEEK },
+  { id: Views.MONTH, label: 'Month', key: Views.MONTH },
+  { id: Views.AGENDA, label: 'Agenda', key: Views.AGENDA },
+];
