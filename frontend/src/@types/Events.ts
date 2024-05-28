eb1b74327027d429d5fe74b6a067cb46e198ea45
pgm-jefdenie <jefdeniel@icloud.com>
2024-05-28 21:47:57 +0200

    🏷️ extend type

--- frontend/src/@types/Events.ts
+++ frontend/src/@types/Events.ts
@@ -1,12 +1,16 @@
 export interface Event {
   id?: number;
   title?: string;
+  description?: string;
   start: string | Date;
   end: string | Date;
+  is_recurring?: boolean;
   location?: string;
-  status?: string;
   priority?: string;
-  allDay?: boolean;
-  is_new?: boolean;
+  recurrence_days_of_week?: string;
+  recurrence_end_date?: string;
+  recurrence_interval?: number;
+  status?: string;
+  allDay?: boolean; // ADD TO DATABASE
   color?: string;
 }
