757c4325e5908de356d1bf417b82052054a0821a
pgm-jefdenie <jefdeniel@icloud.com>
2024-05-28 19:23:18 +0200

    💄 Create first draft of extended eventcards

--- frontend/src/components/ui/EventCard/EventCard.tsx
+++ frontend/src/components/ui/EventCard/EventCard.tsx
@@ -1,35 +1,96 @@
+import { Event } from '../../../@types/Events';
 import ColorConversion from '../../../utils/ColorConversion';
+import {
+  VeryLowPriorityIcon,
+  LowPriorityIcon,
+  MediumPriorityIcon,
+  HighPriorityIcon,
+  VeryHighPriorityIcon,
+} from '../Icon/PriorityIcons';
 
-import './EventCard.scss';
-
-interface Props {
-  title: string;
+interface EventProps {
+  event: Event;
   color?: string;
   onDoubleClick?: () => void;
 }
 
 // Color has to be in hex format
-const EventCard = ({ title, color, onDoubleClick }: Props) => {
+const EventCard = ({
+  event: { title, location, priority, status },
+  color,
+  onDoubleClick,
+}: EventProps) => {
   const defaultColor = ColorConversion.convertHexToRGBA('#141C57');
   const fadedDefaultColor = ColorConversion.convertHexToRGBA('#141C57', 0.15);
   const rgbaColor = color
     ? ColorConversion.convertHexToRGBA(color, 0.2)
     : fadedDefaultColor;
 
+  console.log('Event Props:', { title, location, priority, status });
+
+  let priorityIcon;
+
+  switch (priority) {
+    case 'very_low':
+      priorityIcon = <VeryLowPriorityIcon className="priority-icon" />;
+      break;
+    case 'low':
+      priorityIcon = <LowPriorityIcon className="priority-icon" />;
+      break;
+    case 'medium':
+      priorityIcon = <MediumPriorityIcon className="priority-icon" />;
+      break;
+    case 'high':
+      priorityIcon = <HighPriorityIcon className="priority-icon" />;
+      break;
+    case 'very_high':
+      priorityIcon = <VeryHighPriorityIcon className="priority-icon" />;
+      break;
+    default:
+      priorityIcon = <MediumPriorityIcon className="priority-icon" />;
+  }
+
+  const maxLength = 20;
+  const trimmedlocation =
+    location.length > maxLength
+      ? location.substring(0, maxLength) // Truncate without adding ellipsis
+      : location;
+
   return (
     <div
-      className={`event-card`}
+      className="event-card"
       style={{ backgroundColor: rgbaColor }}
       onDoubleClick={onDoubleClick}
     >
       <div
-        className={`event-card__bar`}
+        className="event-card__bar"
         style={{
           borderLeft: `10px solid ${color ? color : defaultColor}`,
         }}
       ></div>
-
-      <span className={`event-card__title`}>{title}</span>
+      <div>
+        <span className="event-card__title">{title}</span>
+        <div className="event-card__details">
+          {location && (
+            <div className="event-card__location">
+              <strong>Location: </strong>
+              {trimmedlocation}
+            </div>
+          )}
+          {priority && (
+            <div className="event-card__priority">
+              <strong>Priority: </strong>
+              {priorityIcon}
+            </div>
+          )}
+          {status && (
+            <div className="event-card__status">
+              <strong>Status: </strong>
+              {status}
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
