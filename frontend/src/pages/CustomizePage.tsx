0633119666763577ed4352babc466d4c778368c1
pgm-jefdenie <jefdeniel@icloud.com>
2024-05-28 21:49:42 +0200

    🐛 quarantine modal for bug finding

--- frontend/src/pages/CustomizePage.tsx
+++ frontend/src/pages/CustomizePage.tsx
@@ -1,17 +1,37 @@
+import { useState } from 'react';
 import Button from '../components/ui/Button/Button';
 import useSetTitle from '../hooks/setTitle';
+import { useTranslation } from 'react-i18next';
+import AddEventModal from '../components/calendar/events/Modals/AddEventModal';
 
 const CustomizePage = () => {
   useSetTitle('Customize');
+  const [showModal, setShowModal] = useState(false);
+  const { t } = useTranslation(['general', 'customize']);
+  // Modals
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const addEventToList = (event: any) => {
+    console.log('Event:', event);
+  };
+
   return (
     <>
-      <div>
-        <p>Customize your settings here.</p>
-        <Button onClick={() => console.log('Save settings')}>
-          Save settings
-        </Button>
-        <div className="card">test</div>
-      </div>
+      <Button
+        text={t('general:buttons.add')}
+        onClick={openModal}
+        style={{ height: '40px', marginLeft: '25px' }}
+        icon={<img src="/icons/user-plus.svg" alt="add" />}
+      />
+      {showModal && (
+        <AddEventModal setEvent={addEventToList} onClose={closeModal} />
+      )}
     </>
   );
 };
