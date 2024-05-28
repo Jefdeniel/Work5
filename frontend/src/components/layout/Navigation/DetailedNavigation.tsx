2187acb399b09030391ee97a8bbd3cb6c2cc2999
Kyandroo <kyandevo@student.arteveldehs.be>
2024-05-29 01:03:50 +0200

    🎨 Putted imports correct in DetailedNavigation

--- frontend/src/components/layout/Navigation/DetailedNavigation.tsx
+++ frontend/src/components/layout/Navigation/DetailedNavigation.tsx
@@ -1,21 +1,22 @@
 import Logo from '../../ui/Logo';
 import { Col, Row } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import DetailedNavItem from './DetailedNavItem';
 import { Menu, Sidebar } from 'react-pro-sidebar';
 import { Link, useNavigate } from 'react-router-dom';
+
+import DetailedNavItem from './DetailedNavItem';
 import BackButton from '../../ui/Button/BackButton';
 import IconButton from '../../ui/IconButton/IconButton';
 import Calendar from '../../ui/SmallCalendar/SmallCalendar';
-
 import { MenuItem, SideBarProps } from '../../../@types/Nav';
-import './navigation.scss';
 import Icon from '../../ui/Icon/Icon';
 import {
   CustomizeIcon,
   NotificationIcon,
   SharingHubIcon,
 } from '../../ui/Icon/SvgIcons';
+
+import './navigation.scss';
 
 const menuItems: MenuItem[] = [
   {
