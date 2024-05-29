9ba2527ac6dfe1ea23cd97a5ca69c8e29e76e566
Kyandroo <kyandevo@student.arteveldehs.be>
2024-05-29 14:37:34 +0200

    🎨 Added noNav prop to small calendar component

--- frontend/src/components/ui/SmallCalendar/SmallCalendar.tsx
+++ frontend/src/components/ui/SmallCalendar/SmallCalendar.tsx
@@ -7,10 +7,17 @@ type DateRange = [Date | null, Date | null];
 
 interface Props {
   className?: string;
+  noNavigation?: boolean;
   onChange?: (date: Date | DateRange) => void;
+  [key: string]: any; // Allow for any additional props
 }
 
-const SmallCalendar = ({ className, onChange }: Props) => {
+const SmallCalendar = ({
+  className,
+  noNavigation,
+  onChange,
+  ...rest
+}: Props) => {
   const [value, setValue] = useState<Date | DateRange>(new Date());
 
   const handleChange = (date: Date | DateRange) => {
@@ -22,7 +29,8 @@ const SmallCalendar = ({ className, onChange }: Props) => {
 
   return (
     <Calendar
-      className={`${className} full-small-calendar p-1`}
+      {...rest}
+      className={`${className} full-small-calendar p-1 ${noNavigation ? 'no-calendar-navigation' : ''}`}
       onChange={handleChange}
       value={value}
     />
