461c34871e0f2c3d21bd98456ab062cd92b33e3b
pgm-jefdenie <jefdeniel@icloud.com>
2024-05-30 22:37:45 +0200

    ♻️ Create extra endpoint for calendar user

--- backend/calendar_app/views/__init__.py
+++ backend/calendar_app/views/__init__.py
@@ -6,5 +6,7 @@ from .user_views import CustomUserViewSet
 from .user_settings_views import UserSettingsViewSet
 from .category_views import CategoryViewSet
 from .calendar_users_views import CalendarUsersViewSet
+from .calendar_users_views import CalendarUserByUserId
+
 
 default_app_config = "calendar_app.apps.CalendarConfig"
