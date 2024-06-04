3bfbd2c07f2869f0ce24e4ff6b833392a6a7c6ac
pgm-jefdenie <jefdeniel@icloud.com>
2024-06-04 14:51:34 +0200

    ✨ Make agenda events dynamic

--- frontend/src/pages/Calendar/CalendarOverviewPage.tsx
+++ frontend/src/pages/Calendar/CalendarOverviewPage.tsx
@@ -1,11 +1,10 @@
-import { useTranslation } from 'react-i18next';
-import Heading from '../../components/ui/Heading/Heading';
-import useSetTitle from '../../hooks/setTitle';
 import { useContext, useEffect, useState } from 'react';
-import { CalendarContext } from '../../store/CalendarContext';
-import useFetch from '../../hooks/useFetch';
+import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
+import useSetTitle from '../../hooks/setTitle';
+import useFetch from '../../hooks/useFetch';
 import { useSettings } from '../../hooks/useSettings';
+import { CalendarContext } from '../../store/CalendarContext';
 
 const CalendarOverviewPage = () => {
   const { t } = useTranslation(['calendar']);
@@ -19,37 +18,6 @@ const CalendarOverviewPage = () => {
   // /api/calendars/?id=1&id=2&id=3
   // const params = `?id=${userCalendars}`;
   // const calendarIds = userCalendars.map((calendar) => calendar.id).join(',');
-
-  const calendarId = userCalendars[0];
-
-  const { fetchData: getCalendars } = useFetch('GET', [
-    'calendars',
-    // calendarId.toString(),
-  ]);
-
-  useEffect(() => {
-    const fetchCalendars = async () => {
-      const [calendars, setCalendars] = useState<any[]>([]);
-
-      try {
-        if (!userCalendars || userCalendars.length === 0) {
-          return;
-        }
-        const response = await getCalendars();
-        if (response.ok) {
-          const data = await response.json();
-          setCalendars(Array.isArray(data) ? data : [data]);
-        } else {
-          throw new Error('Failed to fetch calendars');
-        }
-      } catch (error) {
-        console.error(t('calendar:error.fetchFailed'), error);
-        toast.error(t('calendar:error.fetchFailed'));
-      }
-    };
-
-    fetchCalendars();
-  }, []);
 
   return (
     <div className={`pattern-block`}>
