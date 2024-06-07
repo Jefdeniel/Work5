5c522ac2e87e827a2098a6e0696df9d0c29ef040
Kyandroo <kyandevo@student.arteveldehs.be>
2024-06-07 15:29:56 +0200

    💄 Made calendar users dynamic from db

--- frontend/src/pages/SharingHubPage.tsx
+++ frontend/src/pages/SharingHubPage.tsx
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Row } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
+
 import { Calendar } from '../@types/Calendar';
+
 import ExcelExportModal from '../components/sharing-hub/Modals/ExcelExportModal';
 import Heading from '../components/ui/Heading/Heading';
 import ActionButtonList from '../components/ui/List/ActionButtonList';
@@ -10,16 +13,41 @@ import {
   EXTERNAL_SERVICES_ITEMS,
 } from '../constants/sharing-hub';
 import useSetTitle from '../hooks/setTitle';
+import Button from '../components/ui/Button/Button';
+import Icon from '../components/ui/Icon/Icon';
+import useFetch from '../hooks/useFetch';
+import LoadingScreen from '../components/ui/Loading/LoadingScreen';
 
 const SharingHubPage = () => {
   const { t } = useTranslation(['calendar']);
   useSetTitle(t('calendar:sharing-hub.title'));
-
-  // State
+  const params = useParams();
+  const calendarId = params.id;
   const [showExcelModal, setShowExcelModal] = useState(false);
-  const calendar: Calendar = {
-    title: '',
-  };
+  const [calendar, setCalendar] = useState<Calendar | null>(null);
+
+  const { fetchData: getCurrentCalendar, loading: isLoading } = useFetch(
+    'GET',
+    [`calendars/${calendarId}`]
+  );
+
+  useEffect(() => {
+    const getCalendar = async () => {
+      try {
+        const response = await getCurrentCalendar();
+        if (response.ok) {
+          const data = await response.json();
+          setCalendar(data);
+        } else {
+          console.error('Error fetching calendar');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getCalendar();
+  }, []);
 
   // Modal handlers
   const openExcelModal = () => setShowExcelModal(true);
@@ -34,6 +62,16 @@ const SharingHubPage = () => {
     handleOutlookClick: () => {},
   };
 
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
+  if (!calendar) {
+    return <div>Loading calendar data...</div>;
+  }
+
+  const calendarUsers = calendar.users;
+
   return (
     <>
       <Row>
@@ -42,8 +80,49 @@ const SharingHubPage = () => {
         </Heading>
         <p className="mb-large">{t('calendar:sharing-hub.description')}</p>
       </Row>
+
       <Row className={`mb-large`}>
-        <Heading level={3}>
+        <Heading level={3} className={`heading heading--sm fw-bold`}>
+          <p>{t('calendar:sharing-hub.usersInAgenda')}</p>
+        </Heading>
+
+        <ul className={`d-flex align-items-center flex-wrap gap-3`}>
+          {calendarUsers &&
+            calendarUsers.map((user) => (
+              <li key={user.id} className={`calendar-users`}>
+                {user.avatar && (
+                  <img
+                    src={user.avatar}
+                    alt={`Profile picture of ${user.first_name}`}
+                  />
+                )}
+
+                {!user.avatar && (
+                  <Icon
+                    src="/icons/user-profile.svg"
+                    alt={`Profile picture of ${user.first_name}`}
+                  />
+                )}
+              </li>
+            ))}
+
+          {/* TODO: Add translations */}
+          {!calendarUsers && <li>No users in this calendar</li>}
+
+          <li>
+            <Button
+              className={`btn--primary mx-2`}
+              icon={<Icon src="/icons/plus-bright.svg" alt="Plus icon" />}
+              // TODO: Add translations
+              text={`Invite`}
+              isSmall
+            />
+          </li>
+        </ul>
+      </Row>
+
+      <Row className={`mb-large`}>
+        <Heading level={3} className={`heading heading--sm fw-bold`}>
           <p>{t('calendar:sharing-hub.exportAgendaAs')}</p>
         </Heading>
         <ActionButtonList
@@ -52,14 +131,9 @@ const SharingHubPage = () => {
           className="btn--primary"
         />
       </Row>
-      {/* TODO: Add users in agenda */}
-      {/* <Row className={`mb-large`}>
-        <Heading level={3}>
-          <p>{t('calendar:sharing-hub.usersInAgenda')}</p>
-        </Heading>
-      </Row> */}
+
       <Row>
-        <Heading level={3}>
+        <Heading level={3} className={`heading heading--sm fw-bold`}>
           <p>{t('calendar:sharing-hub.integrateExternal')}</p>
         </Heading>
         <ActionButtonList
@@ -68,7 +142,8 @@ const SharingHubPage = () => {
           className="btn--primary"
         />
       </Row>
-      {showExcelModal && (
+
+      {showExcelModal && calendar && (
         <ExcelExportModal onClose={closeExcelModal} calendar={calendar} />
       )}
     </>
