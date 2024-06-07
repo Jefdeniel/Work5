e837de9fd1d708ea5df42389bc748a08be71cab8
Kyandroo <kyandevo@student.arteveldehs.be>
2024-06-07 17:14:07 +0200

    ♻️ Fetched calendar categories from DB

--- frontend/src/pages/CustomizePage.tsx
+++ frontend/src/pages/CustomizePage.tsx
@@ -1,15 +1,52 @@
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Row } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+
+import { Calendar } from '../@types/Calendar';
 
 import useSetTitle from '../hooks/setTitle';
+import useFetch from '../hooks/useFetch';
 import Heading from '../components/ui/Heading/Heading';
 import LabelColorInput from '../components/customize/inputs/LabelColorInput';
 import TimeBlockingInput from '../components/customize/inputs/TimeBlockingInput';
 import PermissionBoxes from '../components/customize/inputs/PermissionBoxes';
+import LoadingScreen from '../components/ui/Loading/LoadingScreen';
 
 const CustomizePage = () => {
   const { t } = useTranslation(['general', 'customize']);
   useSetTitle(t('calendar:calendar-customize.title'));
+  const params = useParams();
+  const calendarId = params.id;
+  const [calendar, setCalendar] = useState<Calendar | null>(null);
+
+  const { fetchData: getCurrentCalendar, loading: isLoading } = useFetch(
+    'GET',
+    [`calendars/${calendarId}`]
+  );
+
+  // TODO: Add translations
+  useEffect(() => {
+    const getCalendar = async () => {
+      try {
+        const response = await getCurrentCalendar();
+        if (response.ok) {
+          const data = await response.json();
+          setCalendar(data);
+        } else {
+          console.error('Error fetching calendar');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getCalendar();
+  }, []);
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
 
   return (
     <div className={`d-flex flex-column gap-large`}>
@@ -17,7 +54,7 @@ const CustomizePage = () => {
         <Heading level={1} className="heading--lg clr-primary" />
       </Row>
 
-      <LabelColorInput />
+      {calendar && <LabelColorInput calendar={calendar} />}
 
       <TimeBlockingInput />
 
