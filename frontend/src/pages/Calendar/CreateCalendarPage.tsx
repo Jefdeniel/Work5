0e326690994cde6985bebc65cae410fc8c3c3a6a
pgm-jefdenie <jefdeniel@icloud.com>
2024-06-11 17:07:17 +0200

    🐛 Fix add and delete calendars temporay (with loop bug...)

--- frontend/src/pages/Calendar/CreateCalendarPage.tsx
+++ frontend/src/pages/Calendar/CreateCalendarPage.tsx
@@ -2,7 +2,6 @@ import { Col, Row } from 'react-bootstrap';
 import { Field, Form } from 'react-final-form';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
-import { Calendar } from '../../@types/Calendar';
 import Button from '../../components/ui/Button/Button';
 import Heading from '../../components/ui/Heading/Heading';
 import Icon from '../../components/ui/Icon/Icon';
@@ -11,42 +10,43 @@ import useSetTitle from '../../hooks/setTitle';
 import useAuth from '../../hooks/useAuth';
 import useFetch from '../../hooks/useFetch';
 import Validators from '../../utils/Validators';
+import { CalendarContext } from '../../store/CalendarContext';
+import { useContext } from 'react';
+import { Calendar } from '../../@types/Calendar';
 
 const CreateCalendar = () => {
   const { t } = useTranslation(['calendar']);
   const { user_id } = useAuth();
   useSetTitle(t('calendar-create.title'));
+  const calendarContext = useContext(CalendarContext);
 
   const { fetchData: addCalendar, loading: isLoading } = useFetch('POST', [
     'calendars',
   ]);
 
-  const handleAddCalendar = async (values: Calendar) => {
+  const handleAddCalendar = async (values: any) => {
     try {
       const response = await addCalendar(
         {},
         {
           ...values,
-          title: values.title,
-          description: values.description,
           owner_id: user_id,
-          image:
-            'https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTExL3JtNDY3YmF0Y2gyLWNhbGVuZGFyLTAwMS5wbmc.png',
-          date_start: values.date_start?.toISOString()
-            ? values.date_start?.toISOString()
-            : null,
-          date_stop: values.date_stop?.toISOString()
-            ? values.date_stop?.toISOString()
-            : null,
         }
       );
 
       if (response.ok) {
+        const newCalendar = (await response.json()) as Calendar;
         toast.success(t('calendar:toasts.success'));
+        calendarContext.setCalendars((prevCalendars: Calendar[]) => [
+          ...prevCalendars,
+          newCalendar,
+        ]);
+      } else {
+        toast.error(t('calendar:toasts.error'));
       }
     } catch (error) {
       toast.error(t('calendar:toasts.error'));
-      throw new Error('Failed to add calendar');
+      console.error('Failed to add calendar', error);
     }
   };
 
@@ -62,7 +62,7 @@ const CreateCalendar = () => {
         onSubmit={handleAddCalendar}
         render={({ handleSubmit }) => (
           <form className={`event-form`} onSubmit={handleSubmit}>
-            {/* GENERAL */}
+            {/* General */}
             <Heading
               level={2}
               children={t('calendar-create.general')}
@@ -91,14 +91,12 @@ const CreateCalendar = () => {
               )}
             </Field>
 
-            {/* PEOPLE */}
-
+            {/* People */}
             <Heading
               level={2}
               children={t('calendar-create.people')}
               className={`heading--sm clr-primary-400 mb-small`}
             />
-
             <span className={`mb-base`}>
               {t('calendar-create.people-description')}
             </span>
@@ -108,39 +106,21 @@ const CreateCalendar = () => {
 
               <Button
                 className="btn--primary"
-                icon={<Icon src="/icons/plus-bright.svg " alt="Plus icon" />}
+                icon={<Icon src="/icons/plus-bright.svg" alt="Plus icon" />}
                 text={t('calendar-create.invite-btn')}
                 disabled
               />
             </div>
 
-            {/* DATE RANGE */}
-
+            {/* Date Range */}
             <Heading
               level={2}
               children={t('calendar-create.date-range')}
               className={`heading--sm clr-primary-400 mt-small`}
             />
-
             <span className={`mb-base`}>
               {t('calendar-create.date-range-description')}
             </span>
-            {/* 
-            <Field name="daterange" type="checkbox">
-              {({ input }) => (
-                <div className="form-check switch d-flex align-items-center">
-                  <label className="switch-label">
-                    <input
-                      id="switch"
-                      type="checkbox"
-                      className="form-check-input"
-                      {...input}
-                    />
-                    <div className="switch-slider"></div>
-                  </label>
-                </div>
-              )}
-            </Field> */}
 
             <Row className="mt-0">
               <Col sm={12} md={6} className="m-0">
@@ -174,7 +154,7 @@ const CreateCalendar = () => {
             <div className="pad-left-neg mt-4 mb-5 d-flex justify-content-start">
               <Button
                 className="btn--success inline-block"
-                icon={<Icon src="/icons/plus-bright.svg " alt="Plus icon" />}
+                icon={<Icon src="/icons/plus-bright.svg" alt="Plus icon" />}
                 text={t('calendar-create.title')}
                 type="submit"
                 disabled={isLoading}
