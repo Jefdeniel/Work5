4ce46bf184ebc3c8df4e6a9e99e87a0b84710996
pgm-jefdenie <jefdeniel@icloud.com>
2024-06-14 22:37:45 +0200

    13

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,65 +1,76 @@
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
-import react from "eslint-plugin-react";
-import reactRefresh from "eslint-plugin-react-refresh";
-import prettier from "eslint-plugin-prettier";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import react from 'eslint-plugin-react';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-plugin-prettier';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
-    ignores: ["**/.eslintrc.js", "**/node_modules/", "**/build/", "**/dist/"],
-}, ...fixupConfigRules(compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended",
-)), {
+export default [
+  {
+    ignores: ['**/.eslintrc.js', '**/node_modules/', '**/build/', '**/dist/'],
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      'eslint:recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:prettier/recommended'
+    )
+  ),
+  {
     plugins: {
-        react: fixupPluginRules(react),
-        "react-refresh": reactRefresh,
-        prettier: fixupPluginRules(prettier),
-        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+      react: fixupPluginRules(react),
+      'react-refresh': reactRefresh,
+      prettier: fixupPluginRules(prettier),
+      '@typescript-eslint': fixupPluginRules(typescriptEslint),
     },
 
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2020,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
 
-        parserOptions: {
-            project: "./tsconfig.json",
+      parserOptions: {
+        project: './tsconfig.json',
 
-            ecmaFeatures: {
-                jsx: true,
-            },
+        ecmaFeatures: {
+          jsx: true,
         },
+      },
     },
 
     rules: {
-        "prettier/prettier": ["error", {
-            endOfLine: "auto",
-        }],
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
 
-        "import/prefer-default-export": "off",
-        "import/extensions": "off",
+      'import/prefer-default-export': 'off',
+      'import/extensions': 'off',
 
-        "react-refresh/only-export-components": ["warn", {
-            allowConstantExport: true,
-        }],
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true,
+        },
+      ],
 
-        "@typescript-eslint/strict-boolean-expressions": "warn",
-        "@typescript-eslint/no-unsafe-assignment": "warn",
-        "@typescript-eslint/recommended-type-checked": "warn",
+      '@typescript-eslint/strict-boolean-expressions': 'warn',
+      '@typescript-eslint/no-unsafe-assignment': 'warn',
+      '@typescript-eslint/recommended-type-checked': 'warn',
     },
-}];
+  },
+];
