212c0f5b7e204989780493874cd6e30713eed813
Kyandroo <kyandevo@student.arteveldehs.be>
2024-06-14 12:53:36 +0200

    ♻️ Added custom date/time + view formats to calendar constants

--- frontend/src/constants/calendar.ts
+++ frontend/src/constants/calendar.ts
@@ -13,3 +13,29 @@ export const translateViewOptions = (t) => {
     label: t(`calendar:calendar.views.${option.id}`),
   }));
 };
+
+export const getDateFormats = (time_format) => ({
+  // Short time format
+  LT: time_format === '24h' ? 'HH:mm' : 'h A',
+  // Long time format with seconds
+  LTS: time_format === '24h' ? 'HH:mm:ss' : 'h:mm:ss A',
+  // Short date format
+  L: 'YYYY-MM-DD',
+  // Long date format
+  LL: 'MMMM D, YYYY',
+  //  Long date and time format
+  LLL: 'MMMM D, YYYY HH:mm',
+  // Full date and time format with day of the week
+  LLLL: 'dddd, MMMM D, YYYY HH:mm',
+});
+
+export const getCustomFormats = (time_format) => ({
+  timeGutterFormat: time_format === '24h' ? 'HH:mm' : 'h A',
+  dayTimeFormat: time_format === '24h' ? 'HH:mm' : 'h A',
+  weekTimeFormat: time_format === '24h' ? 'HH:mm' : 'h A',
+  agendaTimeFormat: time_format === '24h' ? 'HH:mm' : 'h A',
+  eventTimeRangeFormat: ({ start, end }, culture: string, localizer) =>
+    localizer.format(start, 'HH:mm', culture) +
+    ' - ' +
+    localizer.format(end, 'HH:mm', culture),
+});
