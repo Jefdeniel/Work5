ca56a7faa7720d57a9cdd9f59663a25e82322712
pgm-jefdenie <jefdeniel@icloud.com>
2024-06-14 21:00:00 +0200

    Fix build 1

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -12,12 +12,14 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
+    '@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended', // Make sure this is always the last element in the array.
   ],
   plugins: ['react', 'react-refresh', 'prettier', '@typescript-eslint'],
   ignorePatterns: ['.eslintrc.js', 'node_modules/', 'build/', 'dist/'],
   rules: {
+    'no-floating-promises': 'warning',
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'import/prefer-default-export': 'off',
     'import/extensions': 'off',
