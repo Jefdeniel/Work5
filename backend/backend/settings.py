8aa8ee1ba90e40b44dda33a8a747e75294c830e1
Kyandroo <kyandevo@student.arteveldehs.be>
2024-06-15 09:43:30 +0200

    🚀 DELETE SECRET KEY JEF

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-&c6e-t5ws_$=@a7ydb($ls_u4tkcqg512=r^nuojsw30^%#)k3"
 SECRET_KEY = os.environ("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
